282aba11b86909f35bc2862c9d9b5d2a4156a3f9
MiracleMaker01 <33798300+MiracleMaker01@users.noreply.github.com>
2017-11-19 16:12:25 +0800

    problem1

---
+++ B10402005_B10202213/B10402005_B10202213/B.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B10402005_B10202213
+{
+    class B
+    {
+    }
+}
