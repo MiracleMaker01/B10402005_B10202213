aed0359a39eb5676f1c39e6e4ba4b752342e00f8
weitsen <33798139+weitsen@users.noreply.github.com>
2017-11-21 20:40:19 +0800

    problem2

--- B10402005_B10202213/B10402005_B10202213/Form1.cs
+++ B10402005_B10202213/B10402005_B10202213/Form1.cs
@@ -16,5 +16,21 @@ namespace B10402005_B10202213
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            A obj = new A();
+            int n = obj.randNumber();
+            label1.Text = n.ToString() ;
+            */
+        }
+
+        
     }
 }
