29129ccbf138f35e0d94cec3d27900a88996a26b
MiracleMaker01 <33798300+MiracleMaker01@users.noreply.github.com>
2017-11-24 23:03:06 +0800

    Problem04

--- B10402005_B10202213/B10402005_B10202213/B.cs
+++ B10402005_B10202213/B10402005_B10202213/B.cs
@@ -8,5 +8,23 @@ namespace B10402005_B10202213
 {
     class B
     {
+        public void Prime()
+        {
+            int n = 0;
+
+            IsPrime(n);
+        }
+
+        public Boolean IsPrime(int prime)
+        {
+            for (int j = 2; j <= prime - 1; j++)
+            {
+                if (prime % j == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
--- B10402005_B10202213/B10402005_B10202213/Form1.Designer.cs
+++ B10402005_B10202213/B10402005_B10202213/Form1.Designer.cs
@@ -53,6 +53,7 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(134, 106);
             this.comboBox1.Name = "comboBox1";
